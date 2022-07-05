12ddfbc26a58d945c365c42fdf908f0fe02ac458
goforGUSTO <jamesdoth@gmail.com>
2022-07-05 12:57:28 -0700

    fixed pop() syntax and added unshift & shift method

--- linkedList.js
+++ linkedList.js
@@ -29,7 +29,7 @@ class LinkedList {
 
     push(value) {
         const newNode = new Node(value)
-        if (!this.head) {
+        if(!this.head) {
             this.head = newNode
             this.tail = newNode
         } else {
@@ -41,20 +41,50 @@ class LinkedList {
     }
     
     pop() {
-        if (this.length == 0) {
-            return null
-        }
+        // if(this.length === 0) {
+        //     return undefined
+        // }
+        if(!this.head) return undefined
         let temp = this.head
         let pre = this.head
-        while(temp.next != null) {
+        // while(temp.next != null) {
+        //     pre = temp
+        //     temp = temp.next
+        // }
+        while(temp.next) {
             pre = temp
             temp = temp.next
         }
         this.tail = pre
         this.tail.next = null
         this.length--
-        if (this.length == 0) {
+        if(this.length === 0) {
             this.head = null
+            this.tail = null
+        }
+        return temp
+    }
+    
+    unshift(value) {
+        const newNode = new Node(value)
+        if(!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
+
+    shift() {
+        if(!this.head) return undefined
+        let temp = this.head
+        this.head = this.head.next
+        temp.next = null
+        this.length--
+        if(this.length === 0) {
             this.tail = null
         }
         return temp
@@ -71,5 +101,7 @@ myLinkedList.push(23)
 myLinkedList.push(12)
 myLinkedList.push(5)
 myLinkedList.pop()
+myLinkedList.unshift(1)
+myLinkedList.shift()
 
 myLinkedList.printAll()
