61fde2340ed7e388320751eb508946f6928f8b01
goforGUSTO <jamesdoth@gmail.com>
2022-07-06 07:49:44 -0700

    added unshift & shift method

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -51,12 +51,43 @@ class DoublyLinkedList {
         this.length--
         return temp
     }
+    unshift(value) {
+        const newNode = new Node(value)
+        if(!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            newNode.next = this.head
+            this.head.prev = newNode
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
+    shift() {
+        if(!this.head) {
+            return undefined
+        }
+        let temp = this.head
+        if(this.length === 1) {
+            this.head = null
+            this.tail = null
+        } else {
+            this.head = this.head.next
+            this.head.prev = null
+            temp.next = null
+        }
+        this.length--
+        return temp
+    }
 }
 
 
 let myDoublyLinkedList = new DoublyLinkedList(5)
 myDoublyLinkedList.push(13)
 myDoublyLinkedList.pop()
+myDoublyLinkedList.unshift(3)
+myDoublyLinkedList.shift()
 
 
 myDoublyLinkedList.printAll()
