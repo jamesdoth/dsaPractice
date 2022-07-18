588519ce0a6787dab4463db64782420702ef9358
goforGUSTO <jamesdoth@gmail.com>
2022-07-18 15:49:14 -0700

    added get method

--- hashTable.js
+++ hashTable.js
@@ -20,11 +20,23 @@ class HashTable {
         this.dataMap[index].push([key, value])
         return this
     }
+    get(key) {
+        let index = this._hash(key)
+        if(this.dataMap[index]) {
+            for(let i = 0; i < this.dataMap[index].length; i++) {
+                if (this.dataMap[index][i][0] === key) {
+                    return this.dataMap[index][i][1]
+                }
+            }
+        }
+        return undefined
+    }
 }
 
 let myHashTable = new HashTable()
 myHashTable.set('shoes', 11)
 myHashTable.set('hat', 45)
+console.log(myHashTable.get('hat'))
 
 //need to figure out a way to print hashtable. probably need recursion
 // using chrome dev tools to see results
