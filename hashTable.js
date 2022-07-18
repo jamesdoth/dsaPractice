55c623ca91514922cc77909d49b0e9ee5e8438ad
goforGUSTO <jamesdoth@gmail.com>
2022-07-18 08:42:36 -0700

    adding comments to clarify _hash method

--- hashTable.js
+++ hashTable.js
@@ -2,6 +2,9 @@ class HashTable {
     constructor(size = 7) {
         this.dataMap = new Array(size)
     }
+    // _hash is a method that is only used in other methods
+    // hash table lengths are optimal on prime numbers
+    // the .length of the key + for loop will determine the array position 0-6
     _hash(key) {
         let hash = 0
         for(let i = 0; i < key.length; i++) {
